28b54fe289b3b845b390ef3570caa0b7a2e109e2
krishan21723 <kchhimpa@noon.com>
2021-08-09 00:40:31 +0530

    added initial code for expense list app

---
+++ react-complete-guide/src/components/NewExpense/NewExpense.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+import ExpenseForm from "./ExpenseForm";
+
+import "./NewExpense.css";
+
+const NewExpense = (props) => {
+  const onSaveExpenseDataHandler = (enteredExpenseData) => {
+    const expnseData = {
+      ...enteredExpenseData,
+      id: Math.random.toString(),
+    };
+    props.onAddExpense(expnseData);
+  };
+  return (
+    <div className="new-expense">
+      <ExpenseForm onSaveExpenseData={onSaveExpenseDataHandler}></ExpenseForm>
+    </div>
+  );
+};
+
+export default NewExpense;
