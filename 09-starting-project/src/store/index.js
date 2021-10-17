b143c8a8d4cb6e70bedbea0c4e9d5a8bc46323c6
krishan21723 <krishan21723@gmail.com>
2021-10-17 15:08:31 +0530

    redux with payload

--- 09-starting-project/src/store/index.js
+++ 09-starting-project/src/store/index.js
@@ -7,6 +7,9 @@ const counterReducer = (state = { counter: 0 }, action) => {
   if (action.type === "decrement") {
     return { counter: state.counter - 1 };
   }
+  if (action.type === "increase") {
+    return { counter: state.counter + action.amount };
+  }
   return state;
 };
 
