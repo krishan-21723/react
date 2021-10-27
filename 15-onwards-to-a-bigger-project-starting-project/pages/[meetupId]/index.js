4eeaa04c58b0d0873dff0e201e093328e7b02080
krishan21723 <krishan21723@gmail.com>
2021-10-27 14:03:27 +0530

    static paths

--- 15-onwards-to-a-bigger-project-starting-project/pages/[meetupId]/index.js
+++ 15-onwards-to-a-bigger-project-starting-project/pages/[meetupId]/index.js
@@ -1,15 +1,53 @@
 import { Fragment } from "react";
 import MeetupDetail from "../../components/meetups/MeetupDetail";
 
-function MeetupDetails() {
+function MeetupDetails(props) {
   return (
     <MeetupDetail
-      image="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg"
-      title="First meetup"
-      address="some address, 54321, some city"
-      description="first meetup desscription test blh blah"
+      image={props.meetupData.image}
+      title={props.meetupData.title}
+      address={props.meetupData.address}
+      description={props.meetupData.description}
     ></MeetupDetail>
   );
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+    fallback: false,
+  };
+}
+
+export async function getStaticProps(context) {
+  //fetch data from api
+
+  const meetupId = context.params.meetupId;
+
+  console.log(meetupId);
+  return {
+    props: {
+      meetupData: {
+        id: "m1",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+        title: "First meetup",
+        address: "some address, 54321, some city",
+        description: "first meetup desscription test blah blah",
+      },
+    },
+  };
 }
 
 export default MeetupDetails;
